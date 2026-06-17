e4cab5acb82340f495d4c7f7b776288a87e5d9cf
Vitinho <vitor.galetti2003@gmail.com>
2026-06-17 02:09:16 -0300

    Feat: Criação do Modelo DELETE

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -16,6 +16,9 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class ProductController {
 
@@ -31,15 +34,23 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productModelList = productRepository.findAll();
+        if(!productModelList.isEmpty()) {
+            for (ProductModel product : productModelList) {
+                UUID id = product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productModelList);
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct (@PathVariable(value="id") UUID id) {
         Optional<ProductModel> product0 = productRepository.findById(id);
                 if(product0.isEmpty()) {
-                    return ResponseEntity.status(HttpStatus.OK).body("Product not found.");
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
                 }
+        product0.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
                 return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
 
@@ -48,11 +59,23 @@ public class ProductController {
                                                 @RequestBody @Valid ProductRecordDto productRecordDto) {
         Optional<ProductModel> product0 = productRepository.findById(id);
         if (product0.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.OK).body("Product not found");
+            return ResponseEntity.status(HttpStatus.OK).body("Product not found.");
         }
         var productModel = product0.get();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
-
     }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body("Product not found.");
+        }
+        productRepository.delete(product0.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted sucessfully.");
+    }
+
+
+
 }
--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -3,6 +3,9 @@ package com.example.springboot.models;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.hibernate.boot.jaxb.hbm.internal.RepresentationModeConverter;
+import org.springframework.hateoas.RepresentationModel;
+
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -10,7 +13,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
-public class ProductModel implements Serializable {
+public class ProductModel  extends RepresentationModel<ProductModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
